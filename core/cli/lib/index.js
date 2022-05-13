4c18893f73b34539a9c1cd6f0dbfaccf27beae86
ZhengKe <3119531802@qq.com>
2022-05-13 13:09:21 +0800

    feat: 环境变量检查功能

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,13 +2,14 @@
 
 module.exports = core;
 
+const path = require("path");
 const semver = require("semver");
 const log = require("@zhengke-cli-dev/log");
 const colors = require("colors/safe");
 const userHome = require("user-home");
 const pathExists = require("path-exists").sync;
 const args = require("minimist")(process.argv.slice(2));
-
+const dotenv = require("dotenv");
 const pkg = require("../package.json");
 const constant = require("./const");
 
@@ -19,10 +20,35 @@ function core() {
     checkRoot();
     checkUserHome();
     checkInputArgs();
-    log.verbose("debug", "test debug");
+    checkEnv();
   } catch (e) {
     log.error(e.message);
   }
+}
+
+// 检查环境变量
+function checkEnv() {
+  const dotenvPath = path.resolve(userHome, ".env");
+  if (pathExists(dotenvPath)) {
+    dotenv.config({
+      path: dotenvPath,
+    });
+  }
+  createDefaultConfig();
+  log.verbose("环境变量", process.env.CLI_HOME_PATH);
+}
+
+function createDefaultConfig() {
+  const cliConfig = {
+    home: userHome,
+  };
+  if (process.env.CLI_HOME) {
+    cliConfig["cliHome"] = path.join(userHome, process.env.CLI_HOME);
+  } else {
+    cliConfig["cliHome"] = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+
+  process.env.CLI_HOME_PATH = cliConfig.cliHome;
 }
 
 // 检查入参
