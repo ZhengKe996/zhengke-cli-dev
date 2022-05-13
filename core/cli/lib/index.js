e835691bd92ce612e76bc94c6550a586112b377d
ZhengKe <3119531802@qq.com>
2022-05-13 12:46:48 +0800

    feat: 用户主目录检查

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -5,7 +5,8 @@ module.exports = core;
 const semver = require("semver");
 const log = require("@zhengke-cli-dev/log");
 const colors = require("colors/safe");
-
+const userHome = require("user-home");
+const pathExists = require("path-exists").sync;
 const pkg = require("../package.json");
 const constant = require("./const");
 
@@ -14,8 +15,16 @@ function core() {
     checkPkgVersion();
     checkNodeVersion();
     checkRoot();
+    checkUserHome();
   } catch (e) {
     log.error(e.message);
+  }
+}
+
+// 检查用户目录
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red("当前登录用户主目录不存在!"));
   }
 }
 
@@ -24,6 +33,7 @@ function checkRoot() {
   const rootCheck = require("root-check");
   rootCheck();
 }
+
 // 检查Node版本号
 function checkNodeVersion() {
   // 第一步 获取当前Node版本号
